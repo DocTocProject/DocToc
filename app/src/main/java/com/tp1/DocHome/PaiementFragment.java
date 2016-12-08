55e26e7d02bee9a759d9e7e8816332b8eefa006b
sacrece <pedrosacrece@gmail.com>
2016-12-08 12:00:05 +0100

    "Integrateur / MenuBurger / color + debut paiement

--- app/src/main/java/com/tp1/DocHome/PaiementFragment.java
+++ app/src/main/java/com/tp1/DocHome/PaiementFragment.java
@@ -6,7 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -21,10 +22,16 @@ public class PaiementFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_paiement, container, false);
+
+
+
+
+
     }
+
+
 
 }
