a6a8178f158bd0e7eb00ba865f77d9fc22525db9
sacrece <pedrosacrece@gmail.com>
2016-12-08 12:53:39 +0100

    "Integrateur / MenuBurger / paiement

--- app/src/main/java/com/tp1/DocHome/PaiementFragment.java
+++ app/src/main/java/com/tp1/DocHome/PaiementFragment.java
@@ -6,32 +6,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PaiementFragment extends Fragment {
+public class PaiementFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
 
     public PaiementFragment() {
         // Required empty public constructor
     }
 
-
+    Spinner spinner;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_paiement, container, false);
+        View rootView =inflater.inflate(R.layout.fragment_paiement, container, false);
+
+        spinner = (Spinner) rootView.findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(),R.array.type,R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
 
-
-
+    return rootView;
 
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText=(TextView) view;
+        Toast.makeText(getContext(),"Choissir"+myText.getText(),Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
