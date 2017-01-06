9d4faa55a46331f2ef024a8dde8de39193a33681
evamartinez <eva.martinez.2212@gmail.com>
2017-01-06 16:13:20 +0100

    Chef project / Fix  the register problem mounir ;) /

--- app/src/main/java/com/tp1/DocHome/RegisterActivity.java
+++ app/src/main/java/com/tp1/DocHome/RegisterActivity.java
@@ -15,19 +15,19 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
-    Button bRegister;
-    EditText etName;
-    EditText etAge;
-    EditText etBirthDate;
-    EditText etUsername;
-    EditText etPassword;
-    EditText etPasswordcheck;
-    RadioGroup radiogroup;
-    RadioButton radioButton;
-    Button button;
-    EditText etphone;
-    EditText etmailid;
-    EditText etPass;
+    Button idbouton;
+    RadioButton idradioboutonfemme;
+    RadioButton idradioboutonhomme;
+    RadioGroup idradiogroup;
+    EditText idprenom;
+    EditText idnom;
+    EditText iddatedenaissance;
+    EditText idnumerodetelephone;
+    EditText idmotdepasse;
+    EditText idconfirmationmotdepasse;
+    EditText idemail;
+
+
 
 
     @Override
@@ -40,65 +40,59 @@ public class RegisterActivity extends AppCompatActivity {
         final int month = c.get(Calendar.MONTH);
         final int day = c.get(Calendar.DAY_OF_MONTH);
 
-        etName = (EditText) findViewById(R.id.etName);
-        etAge = (EditText) findViewById(R.id.etNom);
-
-        etBirthDate = (EditText) findViewById(R.id.etBirthDate);
-        etphone = (EditText) findViewById(R.id.etphone);
-        etmailid = (EditText) findViewById(R.id.etmailid);
-        etUsername = (EditText) findViewById(R.id.etUsername);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        etPass = (EditText) findViewById(R.id.etPasswordcheck);
-
+        idprenom = (EditText) findViewById(R.id.idprenom);
+        idnom = (EditText) findViewById(R.id.idnom);
+        iddatedenaissance = (EditText) findViewById(R.id.iddatedenaissance);
+        idnumerodetelephone = (EditText) findViewById(R.id.idnumerodetelephone);
+        idemail = (EditText) findViewById(R.id.idemail);
+        idmotdepasse = (EditText) findViewById(R.id.idmotdepasse);
+        idconfirmationmotdepasse = (EditText) findViewById(R.id.idconfirmationmotdepasse);
+        idradiogroup = (RadioGroup) findViewById(R.id.idradiogroup);
 
 
-        etBirthDate.setOnClickListener(new View.OnClickListener() {
+
+        iddatedenaissance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerDialog birthdate = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        etBirthDate.setText(year + "/ " + month + "/ " + dayOfMonth);
+                        iddatedenaissance.setText(year + "/ " + month + "/ " + dayOfMonth);
                     }
                 }, year, month, day);
-                birthdate.setTitle("Select BirthDate");
+                birthdate.setTitle("Sélectionner votre date de naissance");
                 birthdate.show();
             }
         });
-
-
-        radiogroup = (RadioGroup) findViewById(R.id.radiogroup);
-        //radiogroup.setOnClickListener(this);
 
     }
 
 
 
-    public void OnClickLreg(View v) {
-        bRegister = (Button) findViewById(R.id.bRegister);
-            /*String etNamestr =etName.getText().toString();
-            String etAgestr =etAge.getText().toString();
-            String etUsernamestr =etUsername.getText().toString();
-            String etPasswordstr =etPassword.getText().toString();
-            String etpasswordcheck =etPasswordcheck.getText().toString();
-            String etphonestr =etphone.getText().toString();
-            String etmailidstr =etmailid.getText().toString();
+    public void OnClickreg(View v) {
+        idbouton = (Button) findViewById(R.id.idbouton);
+            String prenom  =idprenom.getText().toString();
+            String nom =idnom.getText().toString();
+            String motdepasse =idmotdepasse.getText().toString();
+            String confirmationmotdepasse =idconfirmationmotdepasse.getText().toString();
+            String telephone =idnumerodetelephone.getText().toString();
+            String email =idemail.getText().toString();
 
 
 
 
-            if (etNamestr.equals("") || etAgestr.equals("") || etUsernamestr.equals("") || etPasswordstr.equals("") || etphonestr.equals("") || etmailidstr.equals("") || etpasswordcheck.equals("")) {
+            if (prenom.equals("") || nom.equals("")  || motdepasse.equals("") || telephone.equals("") || email.equals("") || confirmationmotdepasse.equals("")) {
                 Toast.makeText(this, "Vous avez oublié de remplir des champs", Toast.LENGTH_SHORT).show();
             }
 
             /*if (etpasswordcheck.equals(etPasswordstr)){
                 Toast.makeText(this,"Le mot de passe n'est pas comfirmé", Toast.LENGTH_SHORT);
-            }
+            }*/
 
-            else {*/
+            else {
 
         startActivity(new Intent(this, TutoActivity.class));
     }
-}
+}}
 
 
