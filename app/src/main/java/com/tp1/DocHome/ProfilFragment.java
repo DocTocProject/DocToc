cdac7a404a31c87f7b9810bf821813f36936eb3b
evamartinez <eva.martinez.2212@gmail.com>
2017-01-19 18:51:51 +0100

    Eva / page profil + carnet de santé

--- app/src/main/java/com/tp1/DocHome/ProfilFragment.java
+++ app/src/main/java/com/tp1/DocHome/ProfilFragment.java
@@ -6,6 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 
@@ -14,17 +28,74 @@ import android.view.ViewGroup;
  */
 public class ProfilFragment extends Fragment {
 
+    EditText profil_prenom, profil_nom, profil_sexe, profil_telephone, profil_email;
+    public static final String MYPREFERENCES = "keysante";
+    public static final String NOM = "keynom";
+    public static final String PRENOM = "keyprenom";
+    public static final String SEXE = "keysexe";
+    public static final String TELEPHONE = "keytelephone";
+    public static final String EMAIL = "keyemail";
 
-    public ProfilFragment() {
-        // Required empty public constructor
-    }
+    SharedPreferences sharedPreferences;
 
-
+    String stringnom;
+    String stringprenom;
+    String stringsexe;
+    String stringtelephone;
+    String stringemail;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sharedPreferences = this.getActivity().getSharedPreferences(MYPREFERENCES, Context.MODE_PRIVATE);
+        stringnom = sharedPreferences.getString(NOM, "");
+        stringprenom = sharedPreferences.getString(PRENOM, "");
+        stringsexe = sharedPreferences.getString(SEXE, "");
+        stringtelephone = sharedPreferences.getString(TELEPHONE, "");
+        stringemail = sharedPreferences.getString(EMAIL, "");
+
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        profil_prenom = (EditText) view.findViewById(R.id.prenom);
+        profil_nom = (EditText) view.findViewById(R.id.nom);
+        profil_sexe = (EditText) view.findViewById(R.id.sexe);
+        profil_telephone = (EditText) view.findViewById(R.id.numerodetelephone);
+        profil_email = (EditText) view.findViewById(R.id.email);
+
+
+
+        profil_prenom.setText(stringprenom);
+        profil_nom.setText(stringnom);
+        profil_telephone.setText(stringtelephone);
+        profil_sexe.setText(stringsexe);
+        profil_email.setText(stringemail);
+
+        Button buttonenregistrer = (Button) view.findViewById(R.id.benregistrer);
+        buttonenregistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringprenom = profil_prenom.getText().toString();
+                stringnom = profil_nom.getText().toString();
+                stringsexe = profil_sexe.getText().toString();
+                stringtelephone =profil_telephone.getText().toString();
+                stringemail = profil_email.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(PRENOM, stringprenom);
+                editor.putString(NOM, stringnom);
+                editor.putString(SEXE, stringsexe);
+                editor.putString(TELEPHONE, stringtelephone);
+                editor.putString(EMAIL, stringemail);
+                editor.commit();
+                Toast.makeText(getContext(), "Le profil a été mis à jour", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+
 
 }
--- app/src/main/java/com/tp1/DocHome/RegisterActivity.java
+++ app/src/main/java/com/tp1/DocHome/RegisterActivity.java
@@ -30,8 +30,6 @@ public class RegisterActivity extends AppCompatActivity {
     EditText idemail;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +40,6 @@ public class RegisterActivity extends AppCompatActivity {
         final int month = c.get(Calendar.MONTH);
         final int day = c.get(Calendar.DAY_OF_MONTH);
 
-
-
         idprenom = (EditText) findViewById(R.id.idprenom);
         idnom = (EditText) findViewById(R.id.idnom);
         iddatedenaissance = (EditText) findViewById(R.id.iddatedenaissance);
@@ -52,8 +48,6 @@ public class RegisterActivity extends AppCompatActivity {
         idmotdepasse = (EditText) findViewById(R.id.idmotdepasse);
         idconfirmationmotdepasse = (EditText) findViewById(R.id.idconfirmationmotdepasse);
         idradiogroup = (RadioGroup) findViewById(R.id.idradiogroup);
-
-
 
         iddatedenaissance.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,12 +80,12 @@ public class RegisterActivity extends AppCompatActivity {
 
             if (prenom.equals("") || nom.equals("")  || motdepasse.equals("") || telephone.equals("") || email.equals("") || motdepasse.equals("") || confirmationmotdepasse.equals("")) {
                 Toast.makeText(this, "Vous avez oublié de remplir des champs", Toast.LENGTH_SHORT).show();
-
             }
 
              else if (!motdepasse.equals(confirmationmotdepasse)){
                Toast.makeText(this, "Vos mot de passe sont différents", Toast.LENGTH_SHORT).show();
-        } else {
+
+            } else {
                     SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(this);
                     SharedPreferences.Editor editor = sharedpreferences.edit();
                     editor.putBoolean("connected", true);
