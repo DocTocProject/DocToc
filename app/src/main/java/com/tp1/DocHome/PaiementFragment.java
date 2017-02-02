ea3831bc43690bde45c3133c0c3f5e0761a41c95
Mounir <hamoutelec@gmail.com>
2017-02-02 20:42:23 +0100

    Paiement

--- app/src/main/java/com/tp1/DocHome/PaiementFragment.java
+++ app/src/main/java/com/tp1/DocHome/PaiementFragment.java
@@ -9,9 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,23 +25,30 @@ import java.util.Calendar;
 public class PaiementFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     EditText iddateexpiration;
-
+    EditText idnumCard;
+    Button register;
+    TextView numCard;
+    TextView dateExp;
+    Spinner spinner;
 
     public PaiementFragment() {
         // Required empty public constructor
     }
 
-    Spinner spinner;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView =inflater.inflate(R.layout.fragment_paiement, container, false);
-
+        idnumCard = (EditText) rootView.findViewById(R.id.num_carte);
+        register = (Button) rootView.findViewById(R.id.ajouter);
         spinner = (Spinner) rootView.findViewById(R.id.spinner);
+        numCard = (TextView) rootView.findViewById(R.id.viewCard);
+        dateExp = (TextView) rootView.findViewById(R.id.viewDate);
+
         ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(),R.array.type,R.layout.support_simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-            // start
+        // start
         final Calendar c = Calendar.getInstance();
         final int year = c.get(Calendar.YEAR);
         final int month = c.get(Calendar.MONTH);
@@ -64,16 +71,37 @@ public class PaiementFragment extends Fragment implements AdapterView.OnItemSele
                 }, year, month, day);
                 birthdate.setTitle("Sélectionner votre date d'expiration");
                 birthdate.show();
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener()
+
+        {
+
+            @Override
+            public void onClick(View v) {
+                String description1 = null;
+                String description2 = null;
+
+                description1 = idnumCard.getText().toString();
+                numCard.setText(description1);
+
+                description2 = iddateexpiration.getText().toString();
+                dateExp.setText(description2);
             }
         });
 
         //end
-    return rootView ;
+        return rootView;
 
 
 
     }
 
+    public void OnClickRegister(View view) {
+
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
